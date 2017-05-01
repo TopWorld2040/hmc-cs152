a2ca92f7c48582ea0759b5ae30e55ef08ac36653
Teerapat Jenrungrot <mjenrungrot@hmc.edu>
2017-05-01 10:16:27 -0700

    Add *.png filetype to bash_script

--- server/tools/make_tar.sh
+++ server/tools/make_tar.sh
@@ -2,6 +2,6 @@
 
 echo -n "Please write an output filename (example: output.tar.gz): " 
 read filename
-tar -cvf $filename *.jpg
+tar -cvf $filename *.jpg *.png
 rm *.jpg
 echo "tar successfully filename is " >> $filename
