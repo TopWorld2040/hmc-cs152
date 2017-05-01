a2346cd9e272f4c1553d0c717f0c92f33e068e1b
Teerapat Jenrungrot <mjenrungrot@hmc.edu>
2017-05-01 10:16:47 -0700

    Fix bash_script

--- server/tools/make_tar.sh
+++ server/tools/make_tar.sh
@@ -3,5 +3,5 @@
 echo -n "Please write an output filename (example: output.tar.gz): " 
 read filename
 tar -cvf $filename *.jpg *.png
-rm *.jpg
+rm *.jpg *.png
 echo "tar successfully filename is " >> $filename
